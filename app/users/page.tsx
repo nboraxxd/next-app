979404855256a8f727541cf1b837c9e5f48b17f3
bora <nhatbaoxxd@gmail.com>
2023-12-29 16:06:00 +0700

    Static and Dynamic Rendering

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,14 +1,18 @@
+import Link from 'next/link'
+
 type User = {
   id: number
   name: string
 }
 
 export default async function UsersPage() {
-  const res = await fetch('https://jsonplaceholder.typicode.com/users')
+  const res = await fetch('https://jsonplaceholder.typicode.com/users', { next: { revalidate: 10 } })
   const users: User[] = await res.json()
 
   return (
     <>
+      <Link href="/">Home</Link>
+      <p>{new Date().toLocaleTimeString()}</p>
       <h1 className="text-xl font-semibold">Users</h1>
       <ul>
         {users.map((user) => (
