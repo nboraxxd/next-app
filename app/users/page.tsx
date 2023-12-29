434bf8ff0df51cd7eaee456af48b7c47e00ea862
bora <nhatbaoxxd@gmail.com>
2023-12-29 15:55:21 +0700

    Data Fetching

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,5 +1,20 @@
-export default function UsersPage() {
+type User = {
+  id: number
+  name: string
+}
+
+export default async function UsersPage() {
+  const res = await fetch('https://jsonplaceholder.typicode.com/users')
+  const users: User[] = await res.json()
+
   return (
-    <div>Users</div>
+    <>
+      <h1 className="text-xl font-semibold">Users</h1>
+      <ul>
+        {users.map((user) => (
+          <li key={user.id}>{user.name}</li>
+        ))}
+      </ul>
+    </>
   )
 }
