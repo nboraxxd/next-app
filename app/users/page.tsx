e378fb763bf7bbad2181247ba7e1e7b557bce552
bora <nhatbaoxxd@gmail.com>
2023-12-31 08:25:38 +0700

    Showing Loading UIs

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,4 +1,6 @@
+import { Suspense } from 'react'
 import Link from 'next/link'
+
 import { UserTable } from '@/app/users/UserTable'
 
 type Props = {
@@ -10,10 +12,13 @@ type Props = {
 export default function UsersPage({ searchParams: { sortOrder } }: Props) {
   return (
     <>
-      <Link href="/">Home</Link>
       <h1 className="text-xl font-semibold">Users</h1>
-      <Link href="/users/new" className='btn btn-primary'>New User</Link>
-      <UserTable sortOrder={sortOrder} />
+      <Link href="/users/new" className="btn btn-primary">
+        New User
+      </Link>
+      <Suspense fallback={<p>Loading...</p>}>
+        <UserTable sortOrder={sortOrder} />
+      </Suspense>
     </>
   )
 }
