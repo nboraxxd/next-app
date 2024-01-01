97f21f7948c34bf0528fc64400f138a8827edd8c
bora <nhatbaoxxd@gmail.com>
2024-01-01 09:17:56 +0700

    Updating an Object

--- app/api/users/[id]/route.tsx
+++ app/api/users/[id]/route.tsx
@@ -4,12 +4,26 @@ type Props = {
   params: { id: number }
 }
 
-export function GET(request: NextRequest, { params: { id } }: Props) {
+export function GET(_request: NextRequest, { params: { id } }: Props) {
   // Fetch data from a database
   // If not found, return 404 error
   // Else return the data
-  if (id > 10) return NextResponse.json({error: 'User not found'}, {status: 404})
+  if (id > 10) return NextResponse.json({ error: 'User not found' }, { status: 404 })
 
-  return NextResponse.json({id, name: 'John Doe'})
+  return NextResponse.json({ id, name: 'John Doe' })
+}
 
+export async function PUT(request: NextRequest, { params: { id } }: Props) {
+  // Validate the request body
+  // If invalid, return 400 error
+  // Else fetch user with the given id
+  // If doesn't exist, return 404 error
+  // Else update the user
+  // Return the updated user
+  const body: { name: string } = await request.json()
+  if (!body.name) return NextResponse.json({ error: 'Name is required' }, { status: 400 })
+
+  if (id > 10) return NextResponse.json({ error: 'User not found' }, { status: 404 })
+
+  return NextResponse.json({ id, name: body.name })
 }
