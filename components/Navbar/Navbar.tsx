ae0b534769940cca246587c633039601e418ee6e
bora <nhatbaoxxd@gmail.com>
2024-01-03 08:37:44 +0700

    Configuring Google Provider

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -2,11 +2,12 @@ import Link from 'next/link'
 
 export default function Navbar() {
   return (
-    <nav className="flex">
+    <nav className="flex space-x-2.5">
       <Link href="/" className="px-5 py-2.5">Next.js</Link>
       <Link href="/users" className="px-5 py-2.5">Users</Link>
       <Link href="/admin" className="px-5 py-2.5">Admin</Link>
 
+      <Link href="/api/auth/signin" className="px-5 py-2.5">Sign in</Link>
     </nav>
   )
 }
