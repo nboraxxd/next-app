18e4fb0389bfbf9b52da0483355b1710adf3ee76
bora <nhatbaoxxd@gmail.com>
2024-01-03 16:48:58 +0700

    Understanding Authentication Sessions

---
+++ app/api/auth/token/route.ts
@@ -0,0 +1,8 @@
+import { NextRequest, NextResponse } from 'next/server'
+import { getToken } from 'next-auth/jwt'
+
+export async function GET(req: NextRequest) {
+  const token = await getToken({ req })
+
+  return NextResponse.json(token)
+}
