2aaadf7301748fedc15b9abf6a7bef054b211ee1
bora <nhatbaoxxd@gmail.com>
2024-01-04 13:10:57 +0700

    Styling Emails

--- emails/WelcomeTemplate.tsx
+++ emails/WelcomeTemplate.tsx
@@ -1,15 +1,17 @@
-import { Html, Preview, Body, Container, Text, Link } from '@react-email/components'
+import { Html, Preview, Body, Container, Text, Link, Tailwind } from '@react-email/components'
 
 export default function WelcomeTemplate({ name }: { name: string }) {
   return (
     <Html>
       <Preview>Welcome aboard!</Preview>
-      <Body>
-        <Container>
-          <Text>Hello {name}</Text>
-          <Link href='https://codewithmosh.com'>www.codewithmosh.com</Link>
-        </Container>
-      </Body>
+      <Tailwind>
+        <Body className="bg-white">
+          <Container>
+            <Text className="text-2xl font-bold">Hello {name}</Text>
+            <Link href="https://codewithmosh.com">www.codewithmosh.com</Link>
+          </Container>
+        </Body>
+      </Tailwind>
     </Html>
   )
 }
